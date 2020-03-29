7ec36e0c9c855227d9937b72ff94098719dcad71
Apoorva Singh <apoorvasingh157@gmail.com>
2020-03-29 16:20:29 +0530

    No cloud storage backend is being used so removed collectfast

--- config/settings/production.py
+++ config/settings/production.py
@@ -139,7 +139,7 @@ ANYMAIL = {
 # Collectfast
 # ------------------------------------------------------------------------------
 # https://github.com/antonagestam/collectfast#installation
-INSTALLED_APPS = ["collectfast"] + INSTALLED_APPS  # noqa F405
+# INSTALLED_APPS = ["collectfast"] + INSTALLED_APPS  # noqa F405
 
 # LOGGING
 # ------------------------------------------------------------------------------
