239babf0d0d410e2d54376f4ad188abc30392c8f
Apoorva Singh <apoorvasingh157@gmail.com>
2020-03-29 11:02:11 +0530

    Health stats are visible.

--- fight_covid19/maps/views.py
+++ fight_covid19/maps/views.py
@@ -8,7 +8,7 @@ from django.views.generic import ListView
 from django.views.generic import View
 from django.views.generic.edit import FormView
 from django.http import JsonResponse
-
+from django.db.models import Q
 from fight_covid19.maps import forms
 from fight_covid19.maps.models import HealthEntry
 
@@ -17,17 +17,38 @@ class HomePage(View):
     def get(self, request, *args, **kwargs):
         c = {
             "cases": "N/A",
+            "activeCases": "N/A",
             "todayCases": "N/A",
             "deaths": "N/A",
             "todayDeaths": "N/A",
             "recovered": "N/A",
             "critical": "N/A",
         }
+
+        # Creating health statistics
+        # HealthEntry.objects.filter(fever=True).count()
+        # HealthEntry.objects.filter(cough=True).count()
+        # HealthEntry.objects.filter(difficult_breathing=True).count()
+        sick_people = HealthEntry.objects.filter(
+            Q(fever=True) | Q(cough=True) | Q(difficult_breathing=True)
+        )
+        c["sickPeople"] = sick_people.count()
+        c["totalPeople"] = (
+            HealthEntry.objects.all().order_by("user").distinct("user_id").count()
+        )
+
+        # Fetching data from API
         r = requests.get(settings.COVID19_STATS_API)
         if r.status_code == 200:
             data = r.json()
             india_stats = list(filter(lambda x: x["country"] == "India", data))
-            c = india_stats[0]
+            c["cases"] = india_stats[0]["cases"]
+            c["todayCases"] = india_stats[0]["todayCases"]
+            c["deaths"] = india_stats[0]["deaths"]
+            c["todayDeaths"] = india_stats[0]["todayDeaths"]
+            c["recovered"] = india_stats[0]["recovered"]
+            c["active"] = india_stats[0]["active"]
+            c["critical"] = india_stats[0]["critical"]
 
         return render(request, "pages/home.html", context=c)
 
