43ee2c1a2ef9b03c255c5fac35f1430dede30b12
ramanaditya <adityaraman96@gmail.com>
2020-04-04 01:58:21 +0530

    Code formatted

--- fight_covid19/maps/api/views.py
+++ fight_covid19/maps/api/views.py
@@ -2,9 +2,8 @@ import requests
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db.models import Q
-from rest_framework.response import Response
 from rest_framework import viewsets
-from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from fight_covid19.maps.models import HealthEntry
 from .serializers import HealthEntrySerializer
@@ -30,19 +29,6 @@ class CoronaVirusCasesViewSet(viewsets.ViewSet):
             "recovered": "N/A",
             "critical": "N/A",
         }
-
-        # Creating health statistics
-        # HealthEntry.objects.filter(fever=True).count()
-        # HealthEntry.objects.filter(cough=True).count()
-        # HealthEntry.objects.filter(difficult_breathing=True).count()
-        sick_people = HealthEntry.objects.filter(
-            Q(fever=True) | Q(cough=True) | Q(difficult_breathing=True)
-        )
-        c["sickPeople"] = sick_people.count()
-        c["totalPeople"] = (
-            HealthEntry.objects.all().order_by("user").distinct("user_id").count()
-        )
-
         # Fetching data from API
         r = requests.get(settings.COVID19_STATS_API)
         if r.status_code == 200:
