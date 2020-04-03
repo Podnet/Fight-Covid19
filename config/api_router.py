c4aad32b5029984f6f3389abe2656b13fa8368b2
ramanaditya <adityaraman96@gmail.com>
2020-04-04 01:43:52 +0530

    Health stat and Corona case
    api endpoints for total corona cases as well as the health stats generated by us has been created

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,11 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from fight_covid19.maps.api.views import HealthEntryViewSet
+from fight_covid19.maps.api.views import (
+    HealthEntryViewSet,
+    CoronaVirusCasesViewSet,
+    HealthStatisticsViewSet,
+)
 from fight_covid19.users.api.views import UserViewSet
 
 if settings.DEBUG:
@@ -11,6 +15,8 @@ else:
 
 router.register("users", UserViewSet)
 router.register("healthentry", HealthEntryViewSet)
+router.register("coronacases", CoronaVirusCasesViewSet, "coronacases")
+router.register("healthstat", HealthStatisticsViewSet, "healthstat")
 
 app_name = "api"
 urlpatterns = router.urls
--- fight_covid19/maps/api/serializers.py
+++ fight_covid19/maps/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from fight_covid19.maps.models import HealthEntry
 
 
