7549f6da86fdaeb6cddd01f0cf4f4f76af059e7b
ramanaditya <adityaraman96@gmail.com>
2020-04-04 23:30:37 +0530

    Health entry api corrected

--- fight_covid19/maps/models.py
+++ fight_covid19/maps/models.py
@@ -3,11 +3,17 @@ from django.contrib.auth import get_user_model
 
 
 class HealthEntry(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    fever = models.BooleanField(default=False)
-    cough = models.BooleanField(default=False)
-    difficult_breathing = models.BooleanField(default=False)
-    self_quarantine = models.BooleanField(default=False)
-    latitude = models.DecimalField(max_digits=18, decimal_places=15)
-    longitude = models.DecimalField(max_digits=18, decimal_places=15)
-    creation_timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, null=True, blank=True
+    )
+    fever = models.BooleanField(default=False, null=True, blank=True)
+    cough = models.BooleanField(default=False, null=True, blank=True)
+    difficult_breathing = models.BooleanField(default=False, null=True, blank=True)
+    self_quarantine = models.BooleanField(default=False, null=True, blank=True)
+    latitude = models.DecimalField(
+        max_digits=18, decimal_places=15, null=True, blank=True
+    )
+    longitude = models.DecimalField(
+        max_digits=18, decimal_places=15, null=True, blank=True
+    )
+    creation_timestamp = models.DateTimeField(auto_now_add=True, null=True, blank=True)
