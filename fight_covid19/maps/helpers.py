1c57dbfa63015fdf55a71bf5be3f514ebd93ddd2
abhi204 <abhi.chaurasia204@gmail.com>
2020-04-05 01:29:33 +0530

    create get_range_coords using GeoLocation

--- fight_covid19/maps/helpers.py
+++ fight_covid19/maps/helpers.py
@@ -2,6 +2,7 @@ import requests
 from django.conf import settings
 from django.db.models import Q
 from fight_covid19.maps.models import HealthEntry
+from fight_covid19.maps.utils import GeoLocation
 
 
 def get_stats():
@@ -28,3 +29,21 @@ def get_map_markers():
         .values("user_id", "latitude", "longitude")
     )
     return list(points)
+
+
+
+def get_range_coords(deg_lat, deg_long, radius=5):
+    '''
+    Takes coordinates in degrees with optional radius value (default=5)
+    Returns a list of min & max longitudes and latitudes
+    '''
+    location = GeoLocation.from_degrees(deg_lat, deg_long)
+    sw_pos, ne_pos = location.distance(radius)
+    # X => long
+    # Y => lat
+    return {
+        'min_lon': sw_pos.deg_lon, 
+        'min_lat': sw_pos.deg_lat,
+        'max_lon': ne_pos.deg_lon,
+        'max_lat': ne_pos.deg_lat,
+    }
