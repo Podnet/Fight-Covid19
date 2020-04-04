0bc20f7f72c50d38b0713c5f211e88282e0d55aa
Apoorva Singh <apoorvasingh157@gmail.com>
2020-04-04 20:09:34 +0530

    Added logo, blacked

--- fight_covid19/news/helpers.py
+++ fight_covid19/news/helpers.py
@@ -2,10 +2,10 @@ from newsapi import NewsApiClient
 
 newsapi = NewsApiClient(api_key="9b1b5c60f90e4b229eced1f999e02310")
 
+
 def get_news(search="coronavirus"):
     top_news = newsapi.get_top_headlines(
-        q=search,
-        sources="bbc-news,the-verge,the-times-of-india"
+        q=search, sources="bbc-news,the-verge,the-times-of-india"
     )
 
     articles = top_news["articles"]
@@ -19,7 +19,7 @@ def get_news(search="coronavirus"):
         desc.append(article["description"])
         img.append(article["urlToImage"])
         url.append(article["url"])
-    
+
     formatted_news = zip(news, desc, img, url)
-        
+
     return formatted_news
