83c176d5f0d082decdf8b79110b12cda439e96e9
Apoorva Singh <apoorvasingh157@gmail.com>
2020-04-06 12:05:36 +0530

    One shot health entry page.

--- fight_covid19/maps/urls.py
+++ fight_covid19/maps/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path("map_markers/", map_views.MapMarkersView, name="map_markers"),
     path("my_health", map_views.MyHealthView, name="my_health"),
     path("patients_nearme", map_views.NearCount.as_view(), name="near_count"),
+    path(
+        "1shot_health_entry",
+        TemplateView.as_view(template_name="maps/one_shot_health_form.html"),
+        name="1shot_health_entry",
+    ),
 ]
