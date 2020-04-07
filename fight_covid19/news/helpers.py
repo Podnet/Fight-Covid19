bf5820df1c4bbbd04c4f7f049edac2cf2c74502c
Hrithik Yadav <hrithiky328@gmail.com>
2020-04-07 22:07:19 +0530

    Update helpers.py

--- fight_covid19/news/helpers.py
+++ fight_covid19/news/helpers.py
@@ -6,7 +6,7 @@ newsapi = NewsApiClient(api_key="9b1b5c60f90e4b229eced1f999e02310")
 def get_news(search="coronavirus"):
     top_news = newsapi.get_top_headlines(
         q=search,
-        sources="bbc-news,the-verge,the-times-of-india,google-news,abc-news,cnn,cnbc,fox-news",
+        sources="bbc-news,the-verge,the-times-of-india,google-news,cnbc",
     )
     news_dict = dict()
     # news = []
