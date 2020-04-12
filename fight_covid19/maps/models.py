1df71086c7afc15d3f860d569e53031440fdcc16
Apoorva Singh <apoorvasingh157@gmail.com>
2020-04-12 18:05:02 +0530

    Created a new HelpEntry model.

--- fight_covid19/maps/models.py
+++ fight_covid19/maps/models.py
@@ -23,3 +23,23 @@ class HealthEntry(models.Model):
 class KeyValuePair(models.Model):
     name = models.CharField(max_length=50)
     value = models.CharField(max_length=100)
+
+
+class HelpEntry(models.Model):
+    fullname = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    help_type = models.CharField(
+        choices=(
+            ("food_donation", "Food Donation"),
+            ("grocery_store", "Grocery Store"),
+            ("medical_store", "Medical Store"),
+            ("other", "Other"),
+        ),
+        default="food_donation",
+        max_length=10,
+    )
+    address = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    latitude = models.DecimalField(max_digits=18, decimal_places=15, null=True)
+    longitude = models.DecimalField(max_digits=18, decimal_places=15, null=True)
+    creation_timestamp = models.DateTimeField(auto_now_add=True, null=True, blank=True)
